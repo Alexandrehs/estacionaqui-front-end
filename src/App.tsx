ce7a25b68ef973398808cefa6f71d0113cf20827
Alexandrehs <echo.alexandre@gmail.com>
2020-08-04 18:42:40 -0300

    Criei a pagina Dashboard, onde ela traz todos carros registrados no banco e tambem por ela consegue inserir e excluir carros. Tem apenas um component criado o Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
 import GlobalStyle from './assets/GlobalStyle';
+import Dashboad from './pages/Dashboar';
 
 function App() {
   return (
     <>
+      <Dashboad />
+
       <GlobalStyle />
     </>
   );
